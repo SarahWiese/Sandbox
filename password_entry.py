d6423775878098b1abbe16684e582645df25c50f
Sarah <sarah.wiese@my.jcu.edu.au>
2019-08-19 16:13:44 +1000

    "Add password check program"

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,23 @@
 """Sarah Wiese"""
+MIN_LENGTH = 5
+
+
+def main():
+    """Program to get and check users password"""
+    password = input(">>> ")
+    print("Enter a valid password: ")
+    while not is_valid_password(password):
+        print("Password must be {} character long".format(MIN_LENGTH))
+        password = input(">>> ")
+    for i in range(len(password)):
+        s = "*"
+    print(i * s)
+
+
+def is_valid_password(password):
+    """Check if passowrd is valid."""
+    if len(password) <= MIN_LENGTH:
+        return False
+        print("Password must be {} characters long".format(MIN_LENGTH))
+        password = input(">>> ")
+    return True
